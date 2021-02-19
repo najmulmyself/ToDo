f6b6bb057860bd7946aaed9b91023ea1b2c6cd01
Najmul Huda <najmul.myself@gmail.com>
2021-02-20 00:12:10 +0600

    initial item works

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let submitBtn = document.getElementById('submit-btn');
 let inputTask = document.getElementById('input-task');
-let deleteBtn = document.getElementsByClassName('btn-complete');
-let deleteItem = document.getElementsByClassName('completed-item')
+let deleteBtn = document.querySelectorAll('.btn-complete');
+let deleteItem = document.querySelectorAll('.completed-item');
+let checkbox = document.querySelectorAll('input[type="checkbox"]');
+let eachItem = document.querySelectorAll('.item');
+let completedItemsUl = document.getElementById('completed-items');
 
 
 function createTask (task){
@@ -33,6 +36,21 @@ submitBtn.addEventListener('click',function(){
 for (let i = 0; i < deleteBtn.length; i++) {
     deleteBtn[i].addEventListener('click',function(){
             deleteItem[i].remove();
+    });
+    
+}
+for (let i = 0; i < checkbox.length; i++) {
+    checkbox[i].addEventListener('click',function(){
+            eachItem[i].remove()
+            let listItem = document.createElement('li');
+            listItem.className = "completed-item";
+            listItem.innerText = eachItem.innerText;
+            let button = document.createElement('button');
+            button.className = "btn btn-complete";
+            button.innerText = 'Delete';
+            listItem.appendChild(button);
+            completedItemsUl.appendChild(listItem);
+
     });
     
 }
