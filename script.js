47063e65fd3a3d7f7fb4461c8bf82d8f95ea09be
Najmul Huda <najmul.myself@gmail.com>
2021-02-20 02:11:06 +0600

    checkbox and delete btn worked

--- script.js
+++ script.js
@@ -5,24 +5,27 @@ let deleteItem = document.querySelectorAll('.completed-item');
 let checkbox = document.querySelectorAll('input[type="checkbox"]');
 let eachItem = document.querySelectorAll('.item');
 let completedItemsUl = document.getElementById('completed-items');
+let getText = document.querySelectorAll('.collect-text');
 
 
-function createTask (task){
-    let listItem = document.createElement('li');
-    let input = document.createElement('input');
-    let label = document.createElement('label');
+// function createTask (task){
+//     let listItem = document.createElement('li');
+//     let input = document.createElement('input');
+//     let label = document.createElement('label');
 
-    label.innerText = task;
-    input.type = 'checkbox';
+//     label.innerText = task;
+//     input.type = 'checkbox';
 
-    listItem.appendChild(input);
-    listItem.appendChild(label);
-    items.appendChild(listItem);
-};
+//     listItem.appendChild(input);
+//     listItem.appendChild(label);
+//     items.appendChild(listItem);
+// };
 submitBtn.addEventListener('click',function(){
     let listItem = document.createElement('li');
+    listItem.className = "item";
     let input = document.createElement('input');
     let label = document.createElement('label');
+    label.className = "collect-text";
 
     label.innerText = ' ' + inputTask.value;
     inputTask.value = '';
@@ -33,24 +36,25 @@ submitBtn.addEventListener('click',function(){
     items.appendChild(listItem);
 })
 
-for (let i = 0; i < deleteBtn.length; i++) {
-    deleteBtn[i].addEventListener('click',function(){
-            deleteItem[i].remove();
-    });
-    
-}
+
 for (let i = 0; i < checkbox.length; i++) {
     checkbox[i].addEventListener('click',function(){
             eachItem[i].remove()
-            let listItem = document.createElement('li');
-            listItem.className = "completed-item";
-            listItem.innerText = eachItem.innerText;
+            let comlistItem = document.createElement('li');
+            comlistItem.className = "completed-item";
+            comlistItem.innerText = eachItem[i].innerText;
             let button = document.createElement('button');
             button.className = "btn btn-complete";
             button.innerText = 'Delete';
-            listItem.appendChild(button);
-            completedItemsUl.appendChild(listItem);
+            comlistItem.appendChild(button);
+            completedItemsUl.appendChild(comlistItem);
 
+    });
+    
+}
+for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click',function(){
+            deleteItem[i].remove();
     });
     
 }
